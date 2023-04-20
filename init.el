f708aeae358ac032ef12352d0833cd05cc19aedb
Lilly Kane <lkane@cornellpump.com>
2023-04-20 10:32:55 -0700

    Bind magit-blame to C-x x b

--- init.el
+++ init.el
@@ -204,6 +204,8 @@
   :custom
   (magit-display-buffer-function #'magit-display-buffer-same-window-except-diff-v1))
 
+(global-set-key (kbd "C-x x b") 'magit-blame)
+
 ;; NOTE: Make sure to configure a GitHub token before using this package!
 ;; - https://magit.vc/manual/forge/Token-Creation.html#Token-Creation
 ;; - https://magit.vc/manual/ghub/Getting-Started.html#Getting-Started
